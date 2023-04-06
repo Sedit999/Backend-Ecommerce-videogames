083729db9301d1f309f7afb0807c6abd64f60d3d
Alan García <alan.ga.g281301@gmail.com>
2023-04-06 01:53:53 -0500

    cambio a getUser

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -14,7 +14,7 @@ const getUsers = async (req, res) => {
 const getUser = async (req, res) => {
   const { username, email } = req.body;
   try {
-    const getUser = await User.findById(req.user.id, { username, email });
+    const getUser = await User.findById(req.user.id, { username });
     res.json(getUser);
   } catch (error) {
     res.status(500).json({ msg: "Hubo un error al actualizar el usuario" });
